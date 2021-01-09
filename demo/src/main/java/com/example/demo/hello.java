318c38b2279dd89cde2e4c9f87fb9235a830eab9
zkj <123@qq.com>
2021-01-09 22:26:25 +0800

    ignore插件的第三次提交

--- demo/src/main/java/com/example/demo/hello.java
+++ demo/src/main/java/com/example/demo/hello.java
@@ -6,6 +6,6 @@ package com.example.demo;
  */
 public class hello {
     public static void main(String[] args) {
-        System.out.println("你好呀。。。。");
+        String str = "hello .ss";
     }
 }
