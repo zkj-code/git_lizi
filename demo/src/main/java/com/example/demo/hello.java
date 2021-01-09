2885a2fc9366cdac922c403de1a850551c0f0586
zkj <123@qq.com>
2021-01-09 22:21:53 +0800

    ignore插件的第三次提交

---
+++ demo/src/main/java/com/example/demo/hello.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+/**
+ * @author ZKJ
+ * @create 2021-01-09 22:20
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("你好呀。。。。");
+    }
+}
